75efedf2c6d25235d0e7453e0fc3ec00b71b6c9d
Bodeeen <andreas.boden@gmail.com>
2017-05-04 13:03:41 +0200

    Arbitrary commit

--- frame_correction.m
+++ frame_correction.m
@@ -3,9 +3,7 @@ function [ output_stack ] = frame_correction( input_stack )
 % and sometimes missing the first "real" frame.
 output_stack = input_stack(:,:,2:end);
 if round(sqrt(size(output_stack, 3))) ~= sqrt(size(output_stack, 3))
-    output_stack = cat(3, input_stack(:,:,1), input_stack);
-else
-    output_stack = input_stack;
+    output_stack = cat(3, output_stack(:,:,1), output_stack);
 end
 
 %% For old scanning
