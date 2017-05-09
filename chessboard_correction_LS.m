b25f74b64aa88bfbba8213402a3c6c212befc72b
Bodeeen <andreas.boden@gmail.com>
2017-05-09 13:44:58 +0200

    Working on the chessboard_corr_LS

--- chessboard_correction_LS.m
+++ chessboard_correction_LS.m
@@ -1,17 +1,29 @@
 
-square_side = 64;
+[LoadFileName,LoadPathName] = uigetfile({'*.*'}, 'Load data file');
+im = imread(strcat(LoadPathName, LoadFileName));
+possible = 20:50;
+r1 = rem(size(im, 1), possible);
+r2 = rem(size(im, 2), possible);
+pos_val1 = find(r1 == 0);
+pos_val2 = find(r2 == 0);
+for v = pos_val1
+    p = find(pos_val2 == v);
+    if p
+        square_side = possible(pos_val2(p));
+    end
+end
 
-% im = ones(2432, 2432);
-im = imread('Data\11pr_rec_Reconstructed_Sthlm.tif');
-im(2:end+1,:) = im;
-% im = corrected_im;
-
+% %temp
+% im = im(1+32*12:40*32, 1+32*12:40*32);
+% square_side = 32;
+% assert(~rem(size(im, 2), square_side), 'Cannot find imside')
 [up_lines, down_lines, right_lines, left_lines] = make_border_matrices(im, square_side);
 
 
 % Find number of coefficients in each dimension
 cx = size(up_lines, 2);
 cy = size(right_lines, 1);
+c = cx*cy;
 
 %% Make left part of "equation matrix"
 
@@ -49,7 +61,9 @@ LR = LR - circshift(LR, -cx, 2);
 UR = UR(:,1:size(UL, 2));
 LR = LR(:,1:size(UL, 2));
 
-A = [UL UR;LL LR];
+A = [UL UR];%;LL LR];
+
+x0 = cat(2, ones(1, c), zeros(1,c));
 
 [U S V] = svd(A'); %http://cmp.felk.cvut.cz/cmp/courses/Y33ROV/Y33ROV_ZS20092010/Lectures/General/constrained_lsq.pdf
 sol = V(:, end);
@@ -59,21 +73,28 @@ O = sol(cx*cy+1:end);
 
 C_mat = reshape(C, [cy cx]);
 C_mat = C_mat';
-O_mat = reshape(O, [cy cx]);
-O_mat = O_mat';
+% O_mat = reshape(O, [cy cx]);
+% O_mat = O_mat';
 
 resize_fac = size(im)./size(C_mat);
 assert(resize_fac(1) == resize_fac(2), 'Something strange here')
 resize_fac = resize_fac(1);
 C_im = imresize(C_mat, resize_fac, 'nearest');
-O_im = imresize(O_mat, resize_fac, 'nearest');
+% O_im = imresize(O_mat, resize_fac, 'nearest');
 
-corrected_right = C_mat .* right_lines + O_mat;
-corrected_left = C_mat .* left_lines + O_mat;
-corrected_im = C_im .* double(im) + O_im;
+corrected_right = C_mat .* right_lines;% + O_mat;
+corrected_left = C_mat .* left_lines;% + O_mat;
+corrected_im = C_im .* double(im);% + O_im;
+
+corrected_im_norm = corrected_im - mean(corrected_im(:));
+corrected_im_inv = -corrected_im_norm;
 
 [n_up_lines, n_down_lines, n_right_lines, n_left_lines] = make_border_matrices(corrected_im, square_side);
-
+figure
+subplot(1,2,1)
+imshow(im,[])
+subplot(1,2,2)
+imshow(corrected_im_inv,[])
 
 
 
