ee476dc2a4e59ba0ec9d2b8abfbdba6f21a96a83
Francesca Pennacchietti <francesca.pennacch@AD.SCILIFELAB.SE>
2017-05-10 10:20:45 +0200

    Got the frame correction file from leastsqbranch

--- frame_correction.m
+++ frame_correction.m
@@ -1,12 +1,14 @@
 function [ output_stack ] = frame_correction( input_stack )
 %% Correct for scanning acquiring one frame in beginning and one too few in the end
 % and sometimes missing the first "real" frame.
-corr_stack = input_stack(:,:,2:end);
-corr_stack(:,:,end+1) = corr_stack(:,:,end);
-if round(sqrt(size(corr_stack, 3))) ~= sqrt(size(corr_stack, 3))
-    output_stack = cat(3, corr_stack(:,:,1), corr_stack);
-else
-    output_stack = corr_stack;
+output_stack = input_stack(:,:,2:end);
+if round(sqrt(size(output_stack, 3))) ~= sqrt(size(output_stack, 3))
+    output_stack = cat(3, output_stack(:,:,1), output_stack);
+end
+
+%% For old scanning
+if round(sqrt(size(output_stack, 3))) ~= sqrt(size(output_stack, 3))
+    output_stack = cat(3, output_stack, output_stack(:,:,end));
 end
 
 output_stack = output_stack - 98; %Remove camera offset
